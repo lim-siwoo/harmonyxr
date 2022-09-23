f436651f6af6e94e7d723d0b356aae3174cae24f
chromato99 <chromato99@gmail.com>
2022-09-23 19:14:33 +0900

    Add project files

---
+++ public/main.js
@@ -0,0 +1,94 @@
+let socket = io("/", {
+  transports: ["websocket"]
+});
+
+let videoGrid = document.getElementById("video-grid");
+let myVideo = document.createElement("video");
+let chatForm = document.getElementById('chatForm');
+myVideo.muted = true;
+
+let username = prompt('Enter username', Math.random());
+let peer = new Peer();
+let conns = new Array();
+
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (peerId) => {
+      connectToNewUser(peerId, stream);
+    });
+  });
+
+peer.on("open", (peerId) => {
+  socket.emit("join-room", roomname, peerId, username);
+});
+
+
+peer.on('connection', function (conn) {
+  conn.on('open', () => {
+    conn.on('data', (data) => {
+      var chatArea = document.getElementById('chatArea');
+      chatArea.append("\n" + data.username + " : " + data.msg);
+      document.getElementById("chatArea").scrollTop = document.getElementById("chatArea").scrollHeight;
+    });
+    conns.push(conn);
+  })
+});
+
+const connectToNewUser = (peerId, stream) => {
+  const call = peer.call(peerId, stream);
+  const conn = peer.connect(peerId);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+
+  conn.on('open', () => {
+    conn.on('data', (data) => {
+      var chatArea = document.getElementById('chatArea');
+      chatArea.append("\n" + data.username + " : " + data.msg);
+      document.getElementById("chatArea").scrollTop = document.getElementById("chatArea").scrollHeight;
+    });
+    conns.push(conn);
+  })
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    videoGrid.append(video);
+  });
+};
+
+// Chat send button onclicked
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var chatInput = document.getElementById('chatInput');
+
+  conns.forEach((conn) => {
+    conn.on('open', () => {
+        conn.send({
+            username: username,
+            msg: chatInput,
+        });
+    })
+  })
+
+  chatInput.value = "";
+});
---
+++ server.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let app = express();
+let port = 8006;
+
+let server = require('http').createServer(app);
+var compression = require('compression')
+
+let io = require('socket.io')(server);
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+
+let roomList = new Array();
+
+app.set('view engine', 'ejs'); // Set render engine mode to ejs
+app.set('views', __dirname + '/views'); // Specify the folder where ejs is located
+
+app.use(compression());
+app.use(express.static('public')); // Set static file location
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index', {roomList: roomList});
+});
+
+app.get('/room/:roomname', (req, res) => {
+  roomList.forEach((elem) => {
+      if(elem.name == req.params.roomname) { // If already exist
+        res.render('room', {roomname: elem.name});
+      }
+  });
+  let room = { // Create new room data
+      name: req.body.roomname,
+  };
+  roomList.push(room);
+  res.render('room', {roomname: room.name});
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomname, peerId, username) => {
+    socket.join(roomname);
+    socket.to(roomname).emit("user-connected", peerId);
+  });
+});
+
+server.listen(port, function () { // Open server
+  console.log(`Listening on https://localhost:${port}/`);
+});
