e233a032942c5f7a40f8004c9ab50b9e63f3a155
doyeon <80394866+vivi108@users.noreply.github.com>
2022-10-11 22:04:02 +0900

    Update disconnection handling (#2)
    * add : disconnect
    
    * pull
    
    * push
    
    * fix : disconnect

--- public/main.js
+++ public/main.js
@@ -9,17 +9,23 @@ myVideo.muted = true;
 
 let username = prompt('Enter username', Math.random());
 let peer = new Peer();
-let conns = new Array();
+let conns = new Array(); // 동접한 사람의 data channel
+let calls = new Array();
 
 let myVideoStream;
+let myPeerId;
+
 navigator.mediaDevices
   .getUserMedia({
     audio: true,
     video: true,
   })
+
   .then((stream) => {
+
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
 
     peer.on("call", (call) => {
       call.answer(stream);
@@ -27,15 +33,47 @@ navigator.mediaDevices
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
+      calls.push({
+        cal : call,
+        video: video
+      });
+
     });
 
     socket.on("user-connected", (peerId) => {
-      connectToNewUser(peerId, stream);
+      connectToNewUser(peerId, stream); // 전화를 주는 클라이언트
     });
   });
 
 peer.on("open", (peerId) => {
   socket.emit("join-room", roomname, peerId, username);
+  myPeerId = peerId;
+
+});
+
+socket.on("user has left",(disPeerId)=> {
+  console.log("0")
+  console.log(calls)
+  for (const key of calls.keys()) {
+    console.log("1")
+    if(calls[key].cal.peer === disPeerId){
+      console.log("2")
+      var removedCall = calls.splice(key, 1);
+      console.log(removedCall[0].video)
+      removedCall[0].video.remove();
+
+      console.log(removedCall[0])
+      removedCall[0].cal.close();
+    }
+  }
+  for (const key of conns.keys()) {
+    if(conns[key].peer === disPeerId){
+      console.log("chatting closed")
+      var removedConn= conns.splice(key, 1);
+      removedConn[0].close();
+    }
+  }
+
 });
 
 
@@ -56,9 +94,13 @@ const connectToNewUser = (peerId, stream) => {
   const conn = peer.connect(peerId);
   const video = document.createElement("video");
   call.on("stream", (userVideoStream) => {
+    console.log("stream")
     addVideoStream(video, userVideoStream);
   });
-
+  calls.push({ // 전화거는쪽
+    cal: call,
+    video: video
+  });
   conn.on('open', () => {
     console.log("DataChannel connected");
     conn.on('data', (data) => {
@@ -66,8 +108,10 @@ const connectToNewUser = (peerId, stream) => {
       chatArea.append("\n" + data.username + " : " + data.msg);
       document.getElementById("chatArea").scrollTop = document.getElementById("chatArea").scrollHeight;
     });
+    
     conns.push(conn);
   })
+
 };
 
 const addVideoStream = (video, stream) => {
@@ -75,6 +119,7 @@ const addVideoStream = (video, stream) => {
   video.addEventListener("loadedmetadata", () => {
     video.play();
     videoGrid.append(video);
+    // video.remove();
   });
 };
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-let express = require('express');
+let express = require('express'); // 웹서버 구축
 let app = express();
 let port = 8006;
 
-let server = require('http').createServer(app);
+let server = require('http').createServer(app); 
 var compression = require('compression')
 
-let io = require('socket.io')(server);
+let io = require('socket.io')(server); // 임포트
 const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
 
 let roomList = new Array();
@@ -23,11 +23,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/room/:roomname', (req, res) => {
-  roomList.forEach((elem) => {
+  for( const elem of roomList) {
     if (elem.name == req.params.roomname) { // If already exist
       res.render('room', { roomname: elem.name });
+      return;
     }
-  });
+  }
+
   let room = { // Create new room data
     name: req.params.roomname,
   };
@@ -39,9 +41,26 @@ io.on('connection', (socket) => {
   socket.on('join-room', (roomname, peerId, username) => {
     socket.join(roomname);
     socket.to(roomname).emit("user-connected", peerId);
+    socket.data.peerId= peerId; // peerid가 생길 때마다 저장
+
   });
+  socket.on("disconnecting", (reason) => {
+    //console.log(socket.id)
+    console.log(socket.rooms)
+     for (const room of socket.rooms){ // 소켓이 속한 룸의 정보
+
+         if (room !== socket.id){
+            console.log(room);
+            io.to(room).emit("user has left", socket.data.peerId);// disconnect된 소켓 제외 나머지 소켓한테 연락
+
+         }
+     }
+    });
 });
 
 server.listen(port, function () { // Open server
   console.log(`Listening on http://localhost:${port}/`);
+
 });
+
+
