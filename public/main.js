8049edc7cfdbce28c1c4f9d1112f953eea818d7a
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2022-10-25 21:59:53 +0900

    Can Change partner attribute and detect collisions (#13)
    * Can Change partner attribute and detect collisions
    
    * Fix conflict between Partner.js except collision
    
    * delete lagacy code
    
    * delete lagacy code.

--- public/main.js
+++ public/main.js
@@ -18,6 +18,7 @@ let controls, group;
 let audioCtx = null;
 let networking;
 
+let v = new THREE.Vector3(); // vector temp for compare collision
 let username = prompt('Enter username', Math.random().toString(36).substring(2, 12));
 // minor pentatonic scale, so whichever notes is striked would be more pleasant
 const musicScale = [0, 3, 5, 7, 10];
@@ -92,6 +93,8 @@ function init() {
     group.position.z = - 0.5;
     scene.add(group);
 
+
+
     const BOXES = 10;
 
     for (let i = 0; i < BOXES; i++) {
@@ -119,11 +122,7 @@ function init() {
         group.add(object);
 
     }
-
     
-
-    //
-
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -229,11 +228,10 @@ function handleCollisions() {
             radius: 0.03,
             center: grip.position
         };
-
+        
         const supportHaptic = 'hapticActuators' in gamepad && gamepad.hapticActuators != null && gamepad.hapticActuators.length > 0;
 
         for (let i = 0; i < group.children.length; i++) {
-
             const child = group.children[i];
             box.setFromObject(child);
             if (box.intersectsSphere(sphere)) {
@@ -296,10 +294,40 @@ function handleCollisions() {
 
 }
 
+// function partnerCollisions(){ //따로 빼야하는데 Partner.js로 빼기힘들어보인다고 생각함.
+
+//     for(let g =0; g < partner.children.length; g++){
+
+//         for (let i = 0; i < group.children.length; i++) {
+//             const child = group.children[i];
+//             box.setFromObject(child);
+//             partner.children[g].getWorldPosition(v);//왜 실제 월드 좌표가 아니라 로컬로 나올까 실제좌표로 되도록 수정해야함.
+//             const sphere = {
+//                 radius: 0.03,
+//                 center: v
+//             };
+//             console.log(v)
+//             if (box.intersectsSphere(sphere)) {//왼손이랑 닿았을때
+//                 console.log("접촉함!!!!")// 제대로 작동함
+//                 child.material.emissive.b = 1;
+//                 const intensity = child.userData.index / group.children.length;
+//                 child.scale.setScalar(1 + Math.random() * 0.1 * intensity);//왜 아무일도 안일어나지?
+//                 // child.position.z -= 0.01; //디버깅용
+//                 // group.position.z -= 0.01; //디버깅용
+//                 const musicInterval = musicScale[child.userData.index % musicScale.length] + 12 * Math.floor(child.userData.index / musicScale.length);
+//                 // oscillators[g].frequency.value = 110 * Math.pow(2, musicInterval / 12);
+//                 // group.children[i].collided = true;
+    
+//             }
+    
+//         }
+//     }
+// }
+
 let cnt = 0;
 
 function render() {
-
+    // partnerCollisions(); //파트너가 실로폰에 닿으면 console에 log가 뜹니다. 하지만 실로폰이 떨리진 않음. 이유는 모르겠습니다...
     handleCollisions();
     if(cnt == 10 ) {
         cnt = 0;
--- public/types/Partner.js
+++ public/types/Partner.js
@@ -8,31 +8,36 @@ class Partner {
 constructor (conn){
     this.conn = conn;
     this.partner = new THREE.Group();
+    this.head = new THREE.Object3D();
+    this.rightHand = new THREE.Object3D();
+    this.leftHand = new THREE.Object3D();
+
 
     // GLTFLOADER
     const loader = new GLTFLoader().setPath('/resources/head/');
     loader.load('scene.gltf', (gltf) => {
-        let partnerHead = gltf.scene;
-        partnerHead.scale.set(1.0, 1.0, 1.0);
-        partnerHead.position.set(0, 1, 0);
+        gltf.scene.scale.set(1.0, 1.0, 1.0);
+        gltf.scene.position.set(0, 1, 0); // 나중에 연동이되면 이 값은 지워줘야합니다.
         let y_angle = 180;
         y_angle = y_angle * 3.14 / 180.0;
-        partnerHead.rotation.set(0, y_angle, 0);
-        this.partner.add(partnerHead);
+        gltf.scene.rotation.set(0, y_angle, 0);
+        this.head.add(gltf.scene);
+        this.partner.add(this.head);
     })
     loader.setPath('/resources/hand/');
     loader.load('scene.gltf', (gltf) => {
-        let partnerHandL = gltf.scene;
-        partnerHandL.scale.set(0.01, 0.01, 0.01);
-        partnerHandL.position.set(0.5, 0.5, 0);
-        this.partner.add(partnerHandL);
+        gltf.scene.scale.set(0.01, 0.01, 0.01);
+        gltf.scene.position.set(0.5, 0.5, 0); //나중에 멀티로 연동이 되면 이 값은 지워야합니다.
+        this.leftHand.add(gltf.scene);
+        this.partner.add(this.leftHand);
     })
 
     loader.load('scene.gltf', (gltf) => {
         let partnerHandR = gltf.scene;
-        partnerHandR.scale.set(0.01, 0.01, 0.01);
-        partnerHandR.position.set(-0.5, 0.5, 0);
-        this.partner.add(partnerHandR);
+        gltf.scene.scale.set(0.01, 0.01, 0.01);
+        gltf.scene.position.set(-0.5, 0.5, 0); //나중에 멀티로 연동이 되면 이 값은 지워야합니다.
+        this.rightHand.add(gltf.scene);
+        this.partner.add(this.rightHand);
     })
 
     // TODO :
@@ -52,6 +57,10 @@ constructor (conn){
         // TODO:
         // 컨트롤러 위치 값도 대입 해줘야함
         //partner.position = data.position;
+        // this.partner.children[1].position = data.head.position; //머리값도 저장해야해요. 로테이션도요.
+        this.partner.children[1].position = data.controller1.position;
+        this.partner.children[2].position = data.controller2.position;
+
       });
 
     });
